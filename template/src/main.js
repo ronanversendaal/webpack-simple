18d6ede29c51b59c2450cb74ef665219d02f7f62
Ronan Versendaal <ronanversendaal@hotmail.com>
2017-12-30 12:33:26 +0100

    edit main.js for font awesome

--- template/src/main.js
+++ template/src/main.js
@@ -3,6 +3,14 @@ import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import "./assets/styles/theme-style.min.css"
+
+import 'vue-awesome/icons'
+import Icon from "vue-awesome/components/Icon.vue"
+
+import "./script.js"
+
+Vue.component('icon', Icon)
 
 Vue.use(BootstrapVue)
 
